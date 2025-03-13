0900499b2d130a97fb099eb2b86eaa927dbe4ae7
Hsu Yadanar <48066007+hsuyadanar@users.noreply.github.com>
2025-03-13 16:18:01 +0630

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async (event) => {
     // Send POST request to FastAPI
     try {
         console.log("in try")
-        const response = await fetch('http://127.0.0.1:8000/predict/', {
+        const response = await fetch('http://127.0.0.1:8888/predict/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
